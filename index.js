42a8c6ab15ab973bd383ba184f7a62fe82314349
Elisa <elisa.naskali@gmail.com>
2020-06-21 13:23:28 +0300

    Add puhelinluettelo 3.1 - 3.8

---
+++ index.js
@@ -0,0 +1,123 @@
+const express = require('express')
+const response = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+morgan.token('postBody', function (req, res) { 
+  return `{"name":"${req.body.name}", "number":"${req.body.number}"}`});
+
+app.use(morgan('tiny', {
+  skip: function(req, res) { 
+    return (req.method === 'POST')}
+}))
+
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.postBody(req, res)
+  ].join(' ')
+}, {
+  skip: function(req, res) { 
+    return (req.method !== 'POST')}
+}))
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-523523523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (req, res) => {
+  const nofPersons = persons.length
+  const time = new Date();
+  res.send(`<div><p>Phonebook has info for ${nofPersons} people</p><p>${time}</p></div>`);
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    res.json(person)    
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
+const generateId = () => {
+  const id = Math.floor(Math.random() * 1024)
+  return id
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  const names = persons.map(p => p.name.toLowerCase())
+
+  if (!body.name) {
+    return res.status(400).json({
+      error: 'name missing'
+    })
+  } else if (!body.number) {
+    return res.status(400).json({
+      error: 'number missing'
+    })
+  } else if (names.includes(body.name.toLowerCase())) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons.concat(person)
+  res.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
